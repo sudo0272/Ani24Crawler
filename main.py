fa055526fbc070908a3bb7967d06751ad5457083
홍세정 <sudo0272@gmail.com>
2019-11-09 15:15:38 +0900

    added: users can select download list

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ ANI24_URL = 'https://ani24zo.com'
 VIDEO_READ_SIZE = 2048
 
 def isNumber(n):
-    return bool(re.match(r'^\d+$'))
+    return bool(re.match(r'^\d+$', n))
 
 searchKeyword = input('검색어: ')
 
@@ -28,7 +28,7 @@ with urllib.request.urlopen(urllib.request.Request('%s/ani/search.php?%s' % (ANI
     
     animeTargetIndex = None
     while True:
-        animeTargetIndex = input()
+        animeTargetIndex = input(' > ')
 
         if isNumber(animeTargetIndex) and 1 <= int(animeTargetIndex) <= len(animeList):
             animeTargetIndex = int(animeTargetIndex) - 1
@@ -41,7 +41,35 @@ with urllib.request.urlopen(urllib.request.Request('%s%s' % (ANI24_URL, animeLis
 
     animeVideoListDivSoup = BeautifulSoup(str(soup.find('div', {'class': 'ani_video_list'})), 'html.parser')
 
-    for i in list(map(str, animeVideoListDivSoup.find_all('a')))[::-1]:
+    animeDownloadParticipants = list(map(str, animeVideoListDivSoup.find_all('a')))
+    animeDownloadParticipants.reverse()
+
+    for i in range(len(animeDownloadParticipants)):
+        print(' %d. %s' % (i + 1, re.search(r'(?<=<div class="subject">).*(?=</div>)', animeDownloadParticipants[i]).group()))
+    
+    print('')
+    print(' ※ 다운로드할 목록을 공백으로 구분해 입력해주세요')
+    print(' ※ 아무것도 입력되지 않으면 모두 다운로드합니다')
+    print('')
+
+    animeDownloadList = []
+    while True:
+        animeDownloadInput = input(' > ')
+        
+        if animeDownloadInput == '':
+            animeDownloadList = animeDownloadParticipants
+
+            break
+
+        for i in animeDownloadInput.split():
+            if isNumber(i) and 1 <= int(i) <= len(animeDownloadParticipants):
+                animeDownloadList.append(animeDownloadParticipants[int(i) - 1])
+        
+        animeDownloadList.sort()
+
+        break
+
+    for i in animeDownloadList:
         videoName = re.search(r'(?<=<div class="subject">).*(?=</div>)', i).group()
         
         with urllib.request.urlopen(urllib.request.Request('%s' % (re.search(r'(?<=<a href=").*(?=">)', i).group()), headers={'User-Agent': 'Mozilla/5.0'})) as videoSite:
